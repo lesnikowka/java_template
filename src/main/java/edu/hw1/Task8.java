28ef6de81566b2c9c2227da78c5b8ad9a8e42395
nik <lesnikowka@mail.ru>
2023-10-14 23:24:12 +0300

    refactor

--- src/main/java/edu/hw1/Task8.java
+++ src/main/java/edu/hw1/Task8.java
@@ -5,16 +5,14 @@ public final class Task8 {
         throw new UnsupportedOperationException("This is a utility class and cannot be instantiated");
     }
 
-    public static boolean allKnightsCannotAttackOtherKnights(int[][] board) throws NullPointerException{
-        if (board == null){
+    public static boolean allKnightsCannotAttackOtherKnights(int[][] board) throws NullPointerException {
+        if (board == null) {
             throw new NullPointerException("Array cannot be null");
         }
 
-        int n = board.length;
-
-        for (int i = 0; i < n; i++) {
-            for (int j = 0; j < n; j++) {
-                if (board[i][j] == 1 && canAttackAnyKnight(board, i, j)){
+        for (int i = 0; i < board.length; i++) {
+            for (int j = 0; j < board.length; j++) {
+                if (board[i][j] == 1 && canAttackAnyKnight(board, i, j)) {
                     return false;
                 }
             }
@@ -30,28 +28,32 @@ public final class Task8 {
 
     private static boolean isKnightOnPlaceLeft(final int[][] board, int x, int y) {
         int n = board.length;
-        int newX = x + 2, newY = y - 1;
+        int newX = x + 2;
+        int newY = y - 1;
 
         return (newX < n) && (newY >= 0) && (board[newX][newY] == 1);
     }
 
     private static boolean isKnightOnPlaceRight(final int[][] board, int x, int y) {
         int n = board.length;
-        int newX = x + 2, newY = y + 1;
+        int newX = x + 2;
+        int newY = y + 1;
 
         return (newX < n) && (newY < n) && (board[newX][newY] == 1);
     }
 
     private static boolean isKnightOnPlaceLeftmost(final int[][] board, int x, int y) {
         int n = board.length;
-        int newX = x + 1, newY = y - 2;
+        int newX = x + 1;
+        int newY = y - 2;
 
         return (newX < n) && (newY >= 0) && (board[newX][newY] == 1);
     }
 
     private static boolean isKnightOnPlaceRightmost(final int[][] board, int x, int y) {
         int n = board.length;
-        int newX = x + 1, newY = y + 2;
+        int newX = x + 1;
+        int newY = y + 2;
 
         return (newX < n) && (newY < n) && (board[newX][newY] == 1);
     }
