d4a29ae6c2f090de23b9115308d3b2aa8deaab5a
nik <lesnikowka@mail.ru>
2023-10-14 23:38:41 +0300

    board correctness check

--- src/main/java/edu/hw1/Task8.java
+++ src/main/java/edu/hw1/Task8.java
@@ -5,9 +5,13 @@ public final class Task8 {
         throw new UnsupportedOperationException("This is a utility class and cannot be instantiated");
     }
 
-    public static boolean allKnightsCannotAttackOtherKnights(int[][] board) throws NullPointerException {
+    public static boolean allKnightsCannotAttackOtherKnights(int[][] board) throws NullPointerException, IllegalArgumentException {
         if (board == null) {
             throw new NullPointerException("Array cannot be null");
+        }
+
+        if (!isBoardCorrect(board)){
+            throw new IllegalArgumentException("Board is incorrect");
         }
 
         for (int i = 0; i < board.length; i++) {
@@ -56,5 +60,37 @@ public final class Task8 {
         int newY = y + 2;
 
         return (newX < n) && (newY < n) && (board[newX][newY] == 1);
+    }
+
+    private static boolean isBoardCorrect(final int[][] board){
+        return isBoardSizeCorrect(board) && doesBoardHaveOnlyOnes(board);
+    }
+
+    private static boolean isBoardSizeCorrect(final int[][] board){
+        final int boardSize = 8;
+
+        if (board.length != boardSize){
+            return false;
+        }
+
+        for (int[] row : board) {
+            if (row == null || row.length != boardSize) {
+                return false;
+            }
+        }
+
+        return true;
+    }
+
+    private static boolean doesBoardHaveOnlyOnes(final int[][] board){
+        for (int[] row : board){
+            for (int element : row){
+                if (element != 1 && element != 0){
+                    return false;
+                }
+            }
+        }
+
+        return true;
     }
 }
