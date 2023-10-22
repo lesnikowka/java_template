8f6b6ccefc27ec499beee8fb2e813a6efd0691d4
nik <lesnikowka@mail.ru>
2023-10-22 20:10:14 +0300

    add Task1

---
+++ src/main/java/edu/hw2/Task1.java
@@ -0,0 +1,113 @@
+package edu.hw2;
+
+import java.lang.Math;
+
+interface Expr {
+    double evaluate();
+
+    public class Constant implements Expr {
+        private final double value;
+
+        public Constant(double value) {
+            this.value = value;
+        }
+
+        public Constant(Expr expr) {
+            this(expr.evaluate());
+        }
+
+        public double evaluate() {
+            return value;
+        }
+    }
+
+    public class Negate implements Expr {
+        private final double value;
+
+        public Negate(double value) {
+            this.value = -value;
+        }
+
+        public Negate(Expr expr) {
+            this(expr.evaluate());
+        }
+
+        public double evaluate() {
+            return value;
+        }
+    }
+
+    public class Exponent implements Expr {
+        private final double value;
+
+        public Exponent(double value1, double value2) {
+            value = Math.pow(value1, value2);
+        }
+
+        public Exponent(Expr expr, double value) {
+            this(expr.evaluate(), value);
+        }
+
+        public Exponent(double value, Expr expr) {
+            this(value, expr.evaluate());
+        }
+
+        public Exponent(Expr expr1, Expr expr2) {
+            this(expr1.evaluate(), expr2.evaluate());
+        }
+
+        public double evaluate() {
+            return value;
+        }
+    }
+
+    public class Addition implements Expr {
+        private final double value;
+
+        public Addition(double value1, double value2) {
+            value = value1 + value2;
+        }
+
+        public Addition(Expr expr, double value) {
+            this(expr.evaluate(), value);
+        }
+
+        public Addition(double value, Expr expr) {
+            this(value, expr.evaluate());
+        }
+
+        public Addition(Expr expr1, Expr expr2) {
+            this(expr1.evaluate(), expr2.evaluate());
+        }
+
+        public double evaluate() {
+            return value;
+        }
+    }
+
+    public class Multiplication implements Expr {
+        private final double value;
+
+        public Multiplication(double value1, double value2) {
+            value = value1 * value2;
+        }
+
+        public Multiplication(Expr expr, double value) {
+            this(expr.evaluate(), value);
+        }
+
+        public Multiplication(double value, Expr expr) {
+            this(value, expr.evaluate());
+        }
+
+        public Multiplication(Expr expr1, Expr expr2) {
+            this(expr1.evaluate(), expr2.evaluate());
+        }
+
+        public double evaluate() {
+            return value;
+        }
+    }
+
+}
+
